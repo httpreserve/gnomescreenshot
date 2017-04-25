ddf0d9293d20e17cb5ae112e4e709756a4e9a623
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-04-25 23:00:10 +1200

    [feature] Output base64 string and write to tmp instead.

--- gnomescreenshot.go
+++ gnomescreenshot.go
@@ -1,33 +1,19 @@
 package gnomescreenshot
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/base64"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
-	"log"
 	"os"
 	"os/exec"
 )
 
-var dir string
+var dir = "/tmp/"
 
-// ErrNoDir is returned when trying to access the screenshot directory
-var ErrNoDir = errors.New("directory not set for output")
-
-// SetDirectoryName must be called before MakeDirectory to allow us to
-// create a directory for screenshots...
-func SetDirectoryName(name string) {
-	dir = name
-}
-
-// MakeDirectory allows us to generate a directory to store screenshots
-func MakeDirectory(dir string) error {
-	if dir == "" {
-		return ErrNoDir
-	}
-
-	return os.Mkdir(dir, 0770)
-}
+// EncodingField tells the browser what we've encoded and how to decode it
+const EncodingField = "data:image/png;base64,"
 
 // getUUID returns a UUID string to be used for naming
 func getUUID() string {
@@ -37,21 +23,17 @@ func getUUID() string {
 
 // GrabScreenshot returns a link to a screenshot for retrieval on localhost
 // and and errors encountered along the way with the calling application
-func GrabScreenshot(link string) (string, string, error) {
+// returns fileloc, command output, error
+func GrabScreenshot(link string) (string, error) {
 
-	if dir != "" {
-		err := MakeDirectory(dir)
-		if err.Error() != "mkdir "+dir+": file exists" {
-			return "", "", err
-		}
-	}
+	//get new filename
+	filename := dir + getUUID() + ".png"
 
 	command := "gnome-web-photo"
 	thumbnail := "--mode=thumbnail"
 	size := "-s"
 	sizeVal := "256"
 	hyperlink := link
-	filename := dir + "/" + getUUID() + ".png"
 
 	args := []string{thumbnail, size, sizeVal, hyperlink, filename}
 	cmd := exec.Command(command, args...)
@@ -61,8 +43,43 @@ func GrabScreenshot(link string) (string, string, error) {
 
 	err := cmd.Run()
 	if err != nil {
-		log.Println(err)
+		return "", errors.Wrap(err, "command run")
 	}
 
-	return filename, out.String(), err
+	b64, err := b64png(filename)
+
+	err = os.Remove(filename)
+	if err != nil {
+		return b64, errors.Wrap(err, "error removing file, b64 may not be nil")
+	}
+
+	return b64, nil
+}
+
+func b64png(filename string) (string, error) {
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	// create a new buffer base on file size
+	fi, _ := f.Stat()
+
+	size := fi.Size()
+	buf := make([]byte, size)
+
+	// read file content into buffer
+	fReader := bufio.NewReader(f)
+	fReader.Read(buf)
+
+	// convert the buffer bytes to base64 string - use buf.Bytes() for new image
+	b64 := base64.StdEncoding.EncodeToString(buf)
+
+	// add data encoding information for html
+	b64 = EncodingField + b64
+
+	// Embed into an html without PNG file
+	return b64, nil
 }
